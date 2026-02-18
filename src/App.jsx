c26b1c8e3c2d2384b4c61e5e05ecdb286f877ca0
Abdul De Borja <abduldbdev@gmail.com>
2026-02-19 01:13:38 +0800

    fix(App): Fix app

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AllEmployeeAccount from "./pages/Accounts/AllAccount";
 import AddNewClient from "./pages/Clients/AddNewClient";
 import AllClients from "./pages/Clients/AllClient";
 import AddNewOrder from "./pages/Orders/AddNewOrder";
+import AllOrders from "./pages/Orders/AllOrders";
 import PatternType from "./pages/DropDownSettings/PatternType/PatternType";
 import AddPatternType from "./pages/DropDownSettings/PatternType/AddPatternType";
 import ApparelType from "./pages/DropDownSettings/ApparelType/ApparelType";
