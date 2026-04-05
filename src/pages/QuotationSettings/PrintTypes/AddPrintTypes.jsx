2f810ac30c098c9ced3aabcff924681ade1b7a96
Abdul De Borja <abduldbdev@gmail.com>
2026-04-05 19:39:29 +0800

    feat(PrintType): Print Type Module

---
+++ src/pages/QuotationSettings/PrintTypes/AddPrintTypes.jsx
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AdminLayout from "../../../layouts/Admin/AdminLayout";
+import Textarea from "../../../components/form/Textarea";
+import FormActions from "../../../components/form/FormActions";
+import Input from "../../../components/form/Input";
+import { quotationTypeInitialState } from "../../../constants/formInitialState/quotationTypeInitialState";
+import { quotationTypeSchema } from "../../../validations/quotationTypeSchema";
+import { validateForm, hasErrors } from "../../../utils/validation";
+import { printTypesApi } from "../../../api/printTypesApi";
+import AlertMessage from "../../../components/common/AlertMessage";
+
+const AddPrintTypes = () => {
+  const navigate = useNavigate();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [formData, setFormData] = useState(quotationTypeInitialState);
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+  const [serverError, setServerError] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    setIsSubmitting(true);
+    setSubmitSuccess(false);
+    setServerError("");
+
+    const validationErrors = validateForm(formData, quotationTypeSchema);
+
+    if (hasErrors(validationErrors)) {
+      setErrors(validationErrors);
+      setIsSubmitting(false);
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      return;
+    }
+
+    try {
+      await printTypesApi.create(formData);
+      setSubmitSuccess(true);
+
+      setFormData(quotationTypeInitialState);
+      setErrors({});
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      setTimeout(() => {
+        navigate(`/quotation/settings/print-types`);
+      }, 1500);
+    } catch (err) {
+      if (err.response?.data?.errors) {
+        setErrors(err.response.data.errors);
+      } else {
+        setServerError(
+          err.response?.data?.message || "Failed to create print types.",
+        );
+      }
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const handleReset = () => {
+    setFormData(quotationTypeInitialState);
+    setErrors({});
+    setSubmitSuccess(false);
+    setServerError("");
+  };
+
+  return (
+    <AdminLayout
+      pageTitle="Add Print Types"
+      path="/quotation/settings/print-types/new"
+      links={[
+        { label: "Home", href: "/" },
+        { label: "Quotation Settings", href: "#" },
+        {
+          label: "Print Types",
+          href: "/quotation/settings/print-types",
+        },
+      ]}
+    >
+      <div className="bg-light p-3 lg:p-7 rounded-lg border border-gray-300">
+        {submitSuccess && (
+          <AlertMessage
+            type="success"
+            title="Print Types created successfully!"
+            message="The new print types has been added to the system."
+          />
+        )}
+
+        {serverError && (
+          <AlertMessage
+            type="error"
+            title={serverError}
+            message="Please check the form and try again."
+          />
+        )}
+        <h1 className="font-semibold text-xl border-b text-primary border-gray-300 pb-2 mb-4">
+          Print Types Details
+        </h1>
+
+        <Input
+          label="Print Types Title"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          error={errors.name}
+          type="text"
+          placeholder="Enter print types name"
+          required
+        />
+
+        <Input
+          label="Base Price"
+          name="base_price"
+          value={formData.base_price}
+          onChange={handleChange}
+          error={errors.base_price}
+          type="text"
+          placeholder="Enter base price"
+          required
+        />
+
+        <Textarea
+          label="Description"
+          name="description"
+          value={formData.description}
+          error={errors.description}
+          onChange={handleChange}
+          rows={15}
+          resizable
+          required
+          placeholder="Enter print types description"
+        />
+      </div>
+
+      <FormActions
+        onSubmit={handleSubmit}
+        onReset={handleReset}
+        isSubmitting={isSubmitting}
+        submitText="Save"
+        resetText="Reset"
+        submittingText="Saving..."
+      />
+    </AdminLayout>
+  );
+};
+
+export default AddPrintTypes;
